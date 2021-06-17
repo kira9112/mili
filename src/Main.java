5afc28311fbfaf2b21cfaea097bca9e744515fc6
Kira9112 <kira91_911@mail.ru>
2021-06-17 16:57:57 +0300

    Initial coomit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    int ticketprice = 10268 ;
+    int onemilyinrub = 20;
+    int mili = ticketprice/onemilyinrub  ;
+        System.out.println(mili);
+    }
+}
+
